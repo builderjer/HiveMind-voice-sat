206f311844b010ecbbf56c6d470ccc7aaeb0d0e7
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2023-06-28 17:14:20 +0100

    v2 rewrite (#21)

--- setup.py
+++ setup.py
@@ -1,34 +1,36 @@
+import os
 from setuptools import setup
+
+BASEDIR = os.path.abspath(os.path.dirname(__file__))
+
+
+
+def required(requirements_file):
+    """ Read requirements file and remove comments and empty lines. """
+    with open(os.path.join(BASEDIR, requirements_file), 'r') as f:
+        requirements = f.read().splitlines()
+        if 'MYCROFT_LOOSE_REQUIREMENTS' in os.environ:
+            print('USING LOOSE REQUIREMENTS!')
+            requirements = [r.replace('==', '>=').replace('~=', '>=') for r in requirements]
+        return [pkg for pkg in requirements
+                if pkg.strip() and not pkg.startswith("#")]
+
+
 
 setup(
     name='HiveMind-voice-sat',
-    version='1.0.8',
-    packages=['mycroft_voice_satellite',
-              'mycroft_voice_satellite.speech',
-              'mycroft_voice_satellite.res',
-              'mycroft_voice_satellite.configuration'],
-    install_requires=["psutil",
-                      "jarbas_hive_mind>=0.10.3",
-                      "pyee",
-                      "requests",
-                      "PyAudio==0.2.11",
-                      "ovos_utils>=0.0.12a9",
-                      "requests_futures",
-                      "json_database>=0.1.3",
-                      "phoneme_guesser>=0.1.0",
-                      "ovos-plugin-manager>=0.0.1",
-                      "ovos-ww-plugin-pocketsphinx>=0.1.0",
-                      "ovos-tts-plugin-responsivevoice>=0.1",
-                      "ovos-stt-plugin-chromium>=0.1.1"],
+    version='2.0.0a1',
+    packages=['hivemind_voice_satellite'],
+    install_requires=required("requirements.txt"),
     include_package_data=True,
     url='https://github.com/OpenJarbas/HiveMind-voice-sat',
     license='MIT',
     author='jarbasAI',
     author_email='jarbasai@mailfence.com',
-    description='Mycroft Voice Satellite',
+    description='Hivemind Voice Satellite',
     entry_points={
         'console_scripts': [
-            'HiveMind-voice-sat=mycroft_voice_satellite.__main__:main'
+            'hivemind-voice-sat=hivemind_voice_satellite.__main__:connect'
         ]
     }
 )
